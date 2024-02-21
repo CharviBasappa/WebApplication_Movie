c6d4137243e8b813d922db114e54b84f28195807
Charvi <charvibasappa98@gmail.com>
2024-02-20 19:59:07 -0600

    Add project files.

---
+++ WebApplication_Movie/Data/Enums/MovieCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication_Movie.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Thriller,
+        Horror,
+        Drama,
+        Documentary,
+        Cartoon
+    }
+}
---
+++ WebApplication_Movie/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_Movie.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actor_Movies { get; set; }
+    }
+}
