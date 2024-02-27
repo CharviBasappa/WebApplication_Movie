f79cc5731dff3126a62120bc6ac0f3e6c0751634
Charvi <charvibasappa98@gmail.com>
2024-02-27 00:01:12 -0600

    Added Movie cards and navigation bar for the controllers created

--- WebApplication_Movie/Program.cs
+++ WebApplication_Movie/Program.cs
@@ -28,7 +28,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 
 using (var scope = app.Services.CreateScope())
 {
