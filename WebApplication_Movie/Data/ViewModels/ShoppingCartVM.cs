ecb659ee01ff79bd2b2d3020be983bf83254a2f7
Charvi <charvibasappa98@gmail.com>
2024-03-13 16:31:32 -0500

    related to cart

---
+++ WebApplication_Movie/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication_Movie.Data.Cart;
+
+namespace WebApplication_Movie.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
